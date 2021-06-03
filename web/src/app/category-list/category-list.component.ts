bfdb635bb5cd15a687f0540dc9a490ed30e5ac57
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-06-03 20:56:19 +0200

    Add category-list display

--- web/src/app/category-list/category-list.component.ts
+++ web/src/app/category-list/category-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../helpers/Category';
+import { NotesService } from '../notes.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(private notesService: NotesService) {}
 
   ngOnInit(): void {
+    this.notesService
+      .getCategories()
+      .subscribe((categories) => (this.categories = categories));
   }
-
 }
--- web/src/app/notes.service.ts
+++ web/src/app/notes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Category } from './helpers/Category';
 import { Note } from './helpers/Note';
 
@@ -8,23 +9,23 @@ import { Note } from './helpers/Note';
 export class NotesService {
   constructor() {}
 
-  saveCategory(cat: Category): boolean {
-    return true;
+  saveCategory(cat: Category): Observable<boolean> {
+    return of(true);
   }
 
-  getCategories(): Category[] {
-    return [
+  getCategories(): Observable<Category[]> {
+    return of([
       new Category('Category1'),
       new Category('Category2'),
       new Category('Category3'),
-    ];
+    ]);
   }
 
-  saveNote(note: Note): boolean {
-    return true;
+  saveNote(note: Note): Observable<boolean> {
+    return of(true);
   }
 
-  getNotes(): Note[] {
+  getNotes(): Observable<Note[]> {
     let cat1 = new Category('Category1');
     cat1.id = 1;
     let cat2 = new Category('Category2');
@@ -32,13 +33,13 @@ export class NotesService {
     let cat3 = new Category('Category3');
     cat3.id = 3;
 
-    return [
+    return of([
       new Note('Note1A', cat1),
       new Note('Note2A', cat2),
       new Note('Note2B', cat2),
       new Note('Note3A', cat3),
       new Note('Note3B', cat3),
       new Note('Note3C', cat3),
-    ];
+    ]);
   }
 }
