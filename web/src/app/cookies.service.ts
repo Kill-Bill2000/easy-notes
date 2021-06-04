31eca450462333a635f479936ce3f125cc1b6df9
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-06-04 13:39:49 +0200

    Implement cookies Service for categories

--- web/src/app/cookies.service.ts
+++ web/src/app/cookies.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Category } from './helpers/Category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CookiesService {
+  private readonly categoryId = 'SelectedCategory';
 
-  constructor() { }
+  constructor(private cookieService: CookieService) {}
+
+  public setCategoryCookie(category: Category) {
+    console.log('Set cookie:');
+    console.log(JSON.stringify(category));
+    if (this.cookieService.check(this.categoryId)) {
+      this.cookieService.delete(this.categoryId);
+    }
+    this.cookieService.set(this.categoryId, JSON.stringify(category));
+  }
+
+  public getCategoryCookie(): Category {
+    if (this.cookieService.check(this.categoryId)) {
+      return JSON.parse(this.cookieService.get(this.categoryId));
+    } else {
+      return null;
+    }
+  }
 }
