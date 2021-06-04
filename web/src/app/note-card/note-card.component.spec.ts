2c9c887d37b996bf9e88123034b372306bd3dfe4
Robin <44874951+Kill-Bill2000@users.noreply.github.com>
2021-06-04 12:46:27 +0200

    Migrate to v11

--- web/src/app/note-card/note-card.component.spec.ts
+++ web/src/app/note-card/note-card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NoteCardComponent } from './note-card.component';
 
@@ -6,7 +6,7 @@ describe('NoteCardComponent', () => {
   let component: NoteCardComponent;
   let fixture: ComponentFixture<NoteCardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NoteCardComponent ]
     })
